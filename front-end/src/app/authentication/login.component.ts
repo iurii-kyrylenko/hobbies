36171038955533d54e91e6fcc3da6ca361d0e5c3
iurii-kyrylenko <yuriproject@gmail.com>
2016-07-23 22:49:59 +0300

    Login page: switched to the model-driven form (for custom validation)

--- front-end/src/app/authentication/login.component.ts
+++ front-end/src/app/authentication/login.component.ts
@@ -1,18 +1,31 @@
-import { Component } from '@angular/core';
-import { NgForm }    from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, REACTIVE_FORM_DIRECTIVES } from '@angular/forms';
 import { AuthService } from './auth.service';
 import { LoginUser } from './user';
 
 @Component({
-    template: require('./login.component.html')
+    template: require('./login.component.html'),
+    directives: [REACTIVE_FORM_DIRECTIVES]
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
-    user = new LoginUser();
+    ctrlEmail: FormControl;
+    ctrlPassword: FormControl;
+    loginForm: FormGroup;
+
+    ngOnInit() {
+        this.ctrlEmail = new FormControl('', [Validators.required, Validators.minLength(5)]);
+        this.ctrlPassword = new FormControl('', Validators.required);
+        this.loginForm = new FormGroup({
+            email: this.ctrlEmail,
+            password: this.ctrlPassword
+        });
+    }
 
     constructor(private auth: AuthService) {}
 
     login() {
-        this.auth.login(this.user, '/home');
+        const user = new LoginUser(this.ctrlEmail.value, this.ctrlPassword.value);
+        this.auth.login(user, '/home');
     }
 }
