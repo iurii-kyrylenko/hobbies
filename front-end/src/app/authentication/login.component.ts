4b47b15c909034d2907a212d19afbc93826d90c5
iurii-kyrylenko <yuriproject@gmail.com>
2016-07-24 10:59:22 +0300

    Revert "Login page: switched to the model-driven form (for custom validation)"
    This reverts commit 36171038955533d54e91e6fcc3da6ca361d0e5c3.

--- front-end/src/app/authentication/login.component.ts
+++ front-end/src/app/authentication/login.component.ts
@@ -1,31 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators, REACTIVE_FORM_DIRECTIVES } from '@angular/forms';
+import { Component } from '@angular/core';
+import { NgForm }    from '@angular/forms';
 import { AuthService } from './auth.service';
 import { LoginUser } from './user';
 
 @Component({
-    template: require('./login.component.html'),
-    directives: [REACTIVE_FORM_DIRECTIVES]
+    template: require('./login.component.html')
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-    ctrlEmail: FormControl;
-    ctrlPassword: FormControl;
-    loginForm: FormGroup;
-
-    ngOnInit() {
-        this.ctrlEmail = new FormControl('', [Validators.required, Validators.minLength(5)]);
-        this.ctrlPassword = new FormControl('', Validators.required);
-        this.loginForm = new FormGroup({
-            email: this.ctrlEmail,
-            password: this.ctrlPassword
-        });
-    }
+    user = new LoginUser();
 
     constructor(private auth: AuthService) {}
 
     login() {
-        const user = new LoginUser(this.ctrlEmail.value, this.ctrlPassword.value);
-        this.auth.login(user, '/home');
+        this.auth.login(this.user, '/home');
     }
 }
