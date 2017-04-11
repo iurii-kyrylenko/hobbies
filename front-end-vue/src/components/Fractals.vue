509e36167abf5d0f18c55bb4f14ddc08bccc10dc
iurii-kyrylenko <yuriproject@gmail.com>
2017-04-11 10:03:58 +0300

    Fractals: simplified

--- front-end-vue/src/components/Fractals.vue
+++ front-end-vue/src/components/Fractals.vue
@@ -1,13 +1,9 @@
 <template>
   <div>
-    <fdraw-r :value="{ width: 320, height: 440 }"></fdraw-r>
-
     <div class="fpanel">
       <input :value="params.width" @input="pushToImmutable('width', $event)" title="Width">
       <span>&times;</span>
       <input :value="params.height" @input="pushToImmutable('height', $event)" title="Height">
-      <a @click="zoom(true)" title="Zoom In">+</a>
-      <a @click="zoom(false)" title="Zoom Out">-</a>
       <span class="info"><b>{{ drawing }}</b></span>
       <div class="info">x:&nbsp;&nbsp;&nbsp;&nbsp;{{ params.x }}</div>
       <div class="info">y:&nbsp;&nbsp;&nbsp;&nbsp;{{ params.y}}</div>
@@ -25,17 +21,17 @@
     components: { FdrawR, FdrawRw },
     mounted () {
       this.$store.commit('notification/notify', {
-        msg: `To move, drag with the mouse or one finger.
-              To zoom in/out, press + - or pinch with two fingers.`,
+        msg: `Move: drag with the mouse or one finger.
+              Zoom in/out: press + - or pinch with two fingers.`,
         type: 'info' })
     },
     data: () => ({
       params: {
-        width: 800,
-        height: 300,
-        x: -1.2627977301623445,
-        y: 0.03909461192195192,
-        zoom: 2487899762
+        width: 320,
+        height: 440,
+        x: -0.5,
+        y: 0,
+        zoom: 150
       },
       drawing: ''
     }),
@@ -46,10 +42,6 @@
           this.params = { ...this.params, [key]: n }
         }
       },
-      zoom (dir) {
-        const zoom = dir ? this.params.zoom * 1.5 : this.params.zoom / 1.5
-        this.params = { ...this.params, zoom }
-      },
       progress (event) {
         this.drawing = event ? 'Drawing...' : ''
       }
@@ -59,46 +51,23 @@
 
 <style scoped>
   .fpanel {
-    margin-top: 16px;
     margin-bottom: 12px;
-    width: 240px;
-    height: 80px;
-    background-color: #f8f8f8;
-    border: 1px solid #C2C2C2;
-    box-shadow: 1px 1px 4px #EBEBEB;
+    width: 325px;
+    height: 90px;
+    border: 1px solid #ccc;
+    box-shadow: 1px 1px 4px #eee;
     border-radius: 3px;
     padding: 2px;
   }
   .fpanel > input {
-    margin: 2px;
-    width: 34px;
-    font-size: 8pt;
-    outline: none;
+    margin: 4px;
+    width: 40px;
     padding: 2px;
-    border: solid 2px #eee;
-    border-radius: 8px;
     text-align: center;
-  }
-  .fpanel > a {
-    text-decoration: none;
-    background-color: #fff;
-    color: #000;
-    cursor: default;
-    width: 20px;
-    height: 20px;
-    margin: 2px;
-    display: inline-block;
-    text-align: center;
-    font-size: 8pt;
-    border: solid 2px #eee;
-    border-radius: 10px;
-  }
-  .fpanel > a:hover {
-    background-color: #eee
   }
   .fpanel > .info {
-    margin-left: 8px;
-    font-family:Courier New;
-    font-size: 8pt;
+    margin-left: 16px;
+    font-family: Courier New;
+    font-size: 0.8em;
   }
 </style>
