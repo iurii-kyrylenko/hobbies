808076060a70fede977ff045f233c7aaf61a693e
iurii-kyrylenko <yuriproject@gmail.com>
2017-05-30 18:30:17 +0300

    back-end: extra content (commented)

--- back-end/server.js
+++ back-end/server.js
@@ -1,49 +1,60 @@
 // Uncomment for local environment or start the server using "heroku local"
-// require('dotenv').config();
+// require('dotenv').config()
 
-require('./config/db');
-require('./config/passport');
+require('./config/db')
+require('./config/passport')
 
-const allowCORS = require('./config/cors');
-const passport = require('passport');
-const path = require('path');
-const express = require('express');
-const bodyParser = require('body-parser');
-const usersRouter = require('./users/router');
-const itemsRouter = require('./items/router');
-const movieInfoController = require('./info/movieController');
+const allowCORS = require('./config/cors')
+const passport = require('passport')
+const path = require('path')
+const express = require('express')
+const bodyParser = require('body-parser')
+const usersRouter = require('./users/router')
+const itemsRouter = require('./items/router')
+const movieInfoController = require('./info/movieController')
 
-const server = express();
+const server = express()
 
-server.use(express.static(path.join(__dirname, 'public')));
+server.use(express.static(path.join(__dirname, 'public')))
 server.use(bodyParser.json())
-server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.urlencoded({ extended: true }))
 
 if(process.env.ALLOW_CORS === 'yes') {
-    allowCORS(server);
+    allowCORS(server)
 }
 
-server.use(passport.initialize());
+server.use(passport.initialize())
 
-/*
- * An artificial delay for debug purpose
- */
+// An artificial delay for debug purpose
+//
 // server.use((req, res, next) => {
-//   setTimeout(() => next(), 1000);
-// });
+//   setTimeout(() => next(), 1000)
+// })
 
-server.use('/api/users', usersRouter);
-server.use('/api/books', itemsRouter.personal('Book'));
-server.use('/api/movies', itemsRouter.personal('Movie'));
-server.use('/api/shared/books', itemsRouter.shared('Book'));
-server.use('/api/shared/movies', itemsRouter.shared('Movie'));
+server.use('/api/users', usersRouter)
+server.use('/api/books', itemsRouter.personal('Book'))
+server.use('/api/movies', itemsRouter.personal('Movie'))
+server.use('/api/shared/books', itemsRouter.shared('Book'))
+server.use('/api/shared/movies', itemsRouter.shared('Movie'))
 // Get info by movie id (qs=imdbId)
-server.get('/api/get-movie', movieInfoController.get);
+server.get('/api/get-movie', movieInfoController.get)
 // Get info by movie title (qs=title)
-server.get('/api/search-movie', movieInfoController.search);
+server.get('/api/search-movie', movieInfoController.search)
+
+// Get extra content
+//
+// const fs = require("fs")
+// server.route(`/${process.env.EXTRA_CONTENT}/:file`)
+// .get((req, res, next) => {
+//   const file = path.join(__dirname, `extra/${req.params.file}`)
+//   fs.exists(file, exists => {
+//     if (exists) res.sendFile(file)
+//     else next()
+//   })
+// })
 
 server.route('/*').get(function(req, res) {
-    return res.sendFile(path.join(__dirname, 'public/index.html'));
-});
+  return res.sendFile(path.join(__dirname, 'public/index.html'))
+})
 
-server.listen(process.env.PORT || 3000);
+server.listen(process.env.PORT || 3000)
