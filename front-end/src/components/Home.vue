bf701d77168b5507b68fa98f3175155af4ed361a
iurii-kyrylenko <yuriproject@gmail.com>
2018-11-03 19:26:44 +0200

    Add support info in Home

--- front-end/src/components/Home.vue
+++ front-end/src/components/Home.vue
@@ -28,18 +28,18 @@
       <p>
           Unathorized people can see and download the shared data.
       </p>
-      <p>
-          <b>This site is for demonstartion purposes only: you can loose data at any time.</b>
-      </p>
       <hr />
       <b>Support</b>
       <p>
         <ul>
           <li>
-            May 25, 2017: Changed movie info service from <a href="http://www.omdbapi.com/" target="_blank">OMDb</a> to <a href="https://www.themoviedb.org/" target="_blank">TMDb</a>
+            Nov 3, 2018: Update to latest versions of Vue.js and other libraries
           </li>
           <li>
-            Apr 24, 2017: Wired up the <a href="http://www.omdbapi.com/" target="_blank">OMDb</a> api service to obtain the movie info
+            May 25, 2017: Change movie info service from <a href="http://www.omdbapi.com/" target="_blank">OMDb</a> to <a href="https://www.themoviedb.org/" target="_blank">TMDb</a>
+          </li>
+          <li>
+            Apr 24, 2017: Wire up the <a href="http://www.omdbapi.com/" target="_blank">OMDb</a> api service to obtain the movie info
           </li>
         </ul>
       </p>
