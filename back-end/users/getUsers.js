08e8f2d530789348307002c0e124f541ec1f37af
iurii-kyrylenko <yuriproject@gmail.com>
2018-11-04 14:34:15 +0200

    Filter out people without hobbies

--- back-end/users/getUsers.js
+++ back-end/users/getUsers.js
@@ -15,24 +15,34 @@ const filterConditions = term => {
   return term ? { $and: [expr, { name: new RegExp(term, 'i') }] } : expr;
 };
 
+const getAggregator = match => User.aggregate()
+  .match(match)
+  .lookup({ from: "books", localField: "_id", foreignField: "userId", as: "books" })
+  .lookup({ from: "movies", localField: "_id", foreignField: "userId", as: "movies" })
+  .project({
+    name: 1,
+    shareBooks: 1,
+    shareMovies: 1,
+    books: { $size: "$books" },
+    movies: { $size: "$movies" },
+    total: { $add: [{ $size: '$books' }, { $size: '$movies' }] }
+  })
+  .match({ total: { $gt: 0 } });
+
 const fetchUsers = async (term, page) => {
   const match = filterConditions(term);
-  const count = await User.count(match);
-  const pages = getPagesCount(count);
 
-  const aggregator = User.aggregate()
-    .match(match)
-    .lookup({ from: "books", localField: "_id", foreignField: "userId", as: "books" })
-    .lookup({ from: "movies", localField: "_id", foreignField: "userId", as: "movies" })
-    .project({
-      name: 1,
-      shareBooks: 1,
-      shareMovies: 1,
-      books: { $size: "$books" },
-      movies: { $size: "$movies" },
-      total: { $add: [{ $size: '$books' }, { $size: '$movies' }] }
-    })
+  const aggregator = getAggregator(match)
     .sort({ total: -1, name: 1 });
+
+  // This doesn't work in mongoose:
+  // getAggregator(match).count("count");
+  const aggregatorCount = getAggregator(match)
+    .append({ $count: "count" });
+
+  const [{ count }] = await aggregatorCount;
+
+  const pages = getPagesCount(count);
 
   if (page > 0) {
     const itemsToSkip = itemsPerPage * (page - 1);
